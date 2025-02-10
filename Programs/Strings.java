14dd66b12394958e7db886cce8d604f2f9367c66
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2025-02-10 12:57:28 +0530

    added strings programs

---
+++ Programs/Strings.java
@@ -0,0 +1,75 @@
+// class Strings{
+//     public static void main(String[] args){
+//         String str = "TEDDY";
+//         String newStr = "";
+//         for(int i = 0; i < str.length(); i++){
+//             char ch = str.charAt(i);
+//             char lower = (char)(ch + 32);
+//             newStr += lower;
+//         }
+//         System.out.println(newStr);
+//     }
+// }
+
+// class Strings{
+//     public static void main(String[] args){
+//         String str = "teddy";
+//         String newStr = "";
+//         for(int i = 0; i < str.length(); i++){
+//             char ch = str.charAt(i);
+//             char lower = (char)(ch - 32);
+//             newStr += lower;
+//         }
+//         System.out.println(newStr);
+//     }
+// }
+
+// class Strings{
+//     public static void main(String[] args){
+//         String str = "xyabbaxzcd";
+//         int[] hash = new int[128];
+//         for(int i = 0; i < str.length(); i++){
+//             char ch = str.charAt(i);
+//             int pos = ch;
+//             hash[pos]++;
+//         }
+//         for(int j = 0; j < hash.length; j++){
+//             if(hash[j] > 0){
+//                 System.out.println((char)j + "->" + hash[j]);
+//             }
+//         }
+//     }
+// }
+
+// class Strings{
+//     public static void main(String[] args){
+//         String str = "abacDd";
+//         int[] hash = new int[128];
+//         for(int i = 0; i < str.length(); i++){
+//             char ch = str.charAt(i);
+//             int pos = ch;
+//             hash[pos]++;
+//         }
+//         for(int j = 0; j < hash.length; j++){
+//             if(hash[j] == 1){
+//                 System.out.println((char)j);
+//             }
+//         }
+//     }
+// }
+
+class Strings{
+    public static void main(String[] args){
+        String str = "abacDd";
+        int[] hash = new int[128];
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            int pos = ch;
+            hash[pos]++;
+        }
+        if(hash[j] > 1){
+            str.replace((char)j, '');
+            System.out.print(str);
+        }
+    }
+}
--- Programs/tempCodeRunnerFile.java
+++ Programs/tempCodeRunnerFile.java
@@ -1,15 +1,1 @@
-class Array{
-    public static void main(String[] args){
-        int[] arr = {2, 5, 7, 6, 3};
-        int first = 0;
-        int last = arr.length - 1;
-        int temp = 0;
-        System.out.println(arr);
-        while(first != last){
-            temp = arr[first];
-            arr[first] = arr[last];
-            arr[last] = temp;
-        }
-        System.out.println(arr);
-    }
-}
+char
